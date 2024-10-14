4385f20e426c7856bcd7c86bfd3a287f99f36a4f
luigiminardim <luigiminardim@gmail.com>
2024-10-14 18:10:13 -0300

    feat: :sparkles: build ord authority

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,138 @@
-use std::{path::Path, sync::Arc};
+use std::{
+    net::SocketAddr, path::{Path, PathBuf}, str::FromStr, sync::Arc
+};
 
 use hickory_server::{
-    authority::{Catalog, ZoneType},
-    proto::rr::{LowerName, Name},
-    store::file::{FileAuthority, FileConfig},
+    authority::{Authority, Catalog, LookupError, LookupOptions, MessageRequest, UpdateResult, ZoneType},
+    proto::rr::{LowerName, Name, RecordType},
+    server::RequestInfo,
+    store::{
+        file::{FileAuthority, FileConfig},
+        recursor::{RecursiveAuthority, RecursiveConfig},
+    },
     ServerFuture,
 };
-use tokio::net;
+use tokio::net::UdpSocket;
 
 #[tokio::main]
 async fn main() {
-    let file_authority = FileAuthority::try_from_config(
-        Name::from_ascii("ord").map_err(|e| println!("{}", e)).unwrap(),
+    let ordinals_authority = OrdAuthority::default();
+
+    // let forward_authority = ForwardAuthority::new(TokioConnectionProvider::default()).unwrap();
+
+    let recursive_authority = RecursiveAuthority::try_from_config(
+        Name::root(),
         ZoneType::Primary,
-        false,
-        Some(Path::new("./data/zone-files")),
-        &FileConfig {
-            zone_file_path: "ord.zone".to_string(),
+        &RecursiveConfig {
+            roots: PathBuf::from("root.zone"),
         },
+        Some(Path::new("./data")),
     )
+    .await
     .unwrap();
 
     let mut catalog = Catalog::new();
-    catalog.upsert(
-        LowerName::new(&Name::from_ascii("ord").unwrap()),
-        Box::new(Arc::new(file_authority)),
-    );
+        catalog.upsert(
+            "ord".parse().unwrap(),
+            Box::new(Arc::new(ordinals_authority)),
+        );
+        // catalog.upsert(Name::root().into(), Box::new(Arc::new(forward_authority)));
+        catalog.upsert("".parse().unwrap(), Box::new(Arc::new(recursive_authority)));
 
-    let mut server: ServerFuture<Catalog> = ServerFuture::new(catalog);
-    let udp_socket = net::UdpSocket::bind("127.0.0.1:1053")
-        .await
-        .expect("could not bind udp socket");
-    server.register_socket(udp_socket);
 
-    server
-        .block_until_done()
-        .await
-        .expect("could not block_until_done");
+    let mut server = ServerFuture::new(catalog);
+    let socket_address: SocketAddr = "0.0.0.0:1053".parse().unwrap();
+    println!("Listening on: {}", socket_address);
+    server.register_socket(UdpSocket::bind(socket_address).await.unwrap());
+    server.block_until_done().await.expect("block_until_done");
+}
+
+struct OrdAuthority {
+    origin: LowerName,
+}
+
+impl OrdAuthority {
+    fn file_authority(&self) -> FileAuthority {
+        FileAuthority::try_from_config(
+            self.origin.clone().into(),
+            ZoneType::Primary,
+            false,
+            Some(Path::new("./data/zone-files")),
+            &FileConfig {
+                zone_file_path: "ord.zone".to_string(),
+            },
+        )
+        .unwrap()
+    }
+}
+
+impl Default for OrdAuthority {
+    fn default() -> Self {
+        Self { origin: LowerName::from_str("ord").unwrap() }
+    }
+}
+
+#[async_trait::async_trait]
+impl Authority for OrdAuthority {
+    type Lookup = <FileAuthority as Authority>::Lookup;
+
+    fn zone_type(&self) -> ZoneType {
+        ZoneType::Primary
+    }
+
+    fn is_axfr_allowed(&self) -> bool {
+        false
+    }
+
+    async fn update(&self, _update: &MessageRequest) -> UpdateResult<bool> {
+        use hickory_server::proto::op::ResponseCode;
+        Err(ResponseCode::NotImp)
+    }
+
+    fn origin(&self) -> &LowerName {
+        &self.origin
+    }
+
+    async fn lookup(
+        &self,
+        name: &LowerName,
+        rtype: RecordType,
+        lookup_options: LookupOptions,
+    ) -> Result<Self::Lookup, LookupError> {
+        println!("lookup: {:?} {:?} {:?}", name, rtype, lookup_options);
+        self.file_authority().lookup(name, rtype, lookup_options).await
+    }
+
+    async fn search(
+        &self,
+        request_info: RequestInfo<'_>,
+        lookup_options: LookupOptions,
+    ) -> Result<Self::Lookup, LookupError> {
+        println!("search: {:?} {:?}", request_info.query, lookup_options);
+        self.file_authority().search(request_info, lookup_options).await
+    }
+
+    async fn ns(&self, lookup_options: LookupOptions) -> Result<Self::Lookup, LookupError> {
+        println!("ns: {:?}", lookup_options);
+        self.file_authority().ns(lookup_options).await
+    }
+
+    async fn get_nsec_records(
+        &self,
+        name: &LowerName,
+        lookup_options: LookupOptions,
+    ) -> Result<Self::Lookup, LookupError> {
+        println!("get_nsec_records: {:?} {:?}", name, lookup_options);
+        self.file_authority().get_nsec_records(name, lookup_options).await
+    }
+
+    async fn soa(&self) -> Result<Self::Lookup, LookupError> {
+        println!("soa");
+        self.file_authority().soa().await
+    }
+
+    async fn soa_secure(&self, lookup_options: LookupOptions) -> Result<Self::Lookup, LookupError> {
+        println!("soa_secure: {:?}", lookup_options);
+        self.file_authority().soa_secure(lookup_options).await
+    }
 }
