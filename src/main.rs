a5c2c93a308c783d17b805039740ddfe24aefcf3
luigiminardim <luigiminardim@gmail.com>
2025-01-11 13:11:23 -0300

    feat: :sparkles: use InMemoryAuthority

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-
 use lib::dns::{
+    core::{AData, Class, Data, Record},
     net::{UdpListener, UdpStubResolver},
-    // resolver::EmptyResolver,
+    resolver::InMemoryAuthority,
 };
+use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    let udp_listener = UdpListener::new(UdpStubResolver::new(SocketAddr::new(
-        IpAddr::V4(Ipv4Addr::new(1, 1, 1, 1)),
-        53,
-    )));
+    let cloud_flare_socket_addr = "1.1.1.1:53".parse::<SocketAddr>().unwrap();
+    let _udp_resolver = UdpStubResolver::new(cloud_flare_socket_addr);
+    let in_memory_authority = InMemoryAuthority::new(vec![Record::new(
+        "ord.".parse().unwrap(),
+        Class::IN,
+        0,
+        Data::A(AData::new("1.2.3.4".parse().unwrap())),
+    )]);
+    let udp_listener = UdpListener::new(in_memory_authority);
     udp_listener
         .listen()
         .await
