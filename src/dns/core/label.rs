a33e7a97167245b26c4723c1706875c838bf7a8e
luigiminardim <luigiminardim@gmail.com>
2025-01-12 12:48:02 -0300

    fix: :bug: fix label validations

--- src/dns/core/label.rs
+++ src/dns/core/label.rs
@@ -28,15 +28,50 @@ impl Label {
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum LabelFromStrErr {
     LenLimit,
+    InvalidChar,
 }
 impl FromStr for Label {
     type Err = LabelFromStrErr;
 
+    /// <label> ::= <letter> [ [ <ldh-str> ] <let-dig> ]
+    /// <ldh-str> ::= <let-dig-hyp> | <let-dig-hyp> <ldh-str>
+    /// <let-dig-hyp> ::= <let-dig> | "-"
+    /// <let-dig> ::= <letter> | <digit>
+    /// <letter> ::= any one of the 52 alphabetic characters A through Z in upper case and a through z in lower case
+    /// <digit> ::= any one of the ten digits 0 through 9
     fn from_str(s: &str) -> Result<Self, Self::Err> {
+        fn is_letter(c: char) -> bool {
+            c.is_ascii_alphabetic()
+        }
+        fn is_digit(c: char) -> bool {
+            c.is_ascii_digit()
+        }
+        fn is_hyphen(c: char) -> bool {
+            c == '-'
+        }
+        fn is_valid_char(c: char) -> bool {
+            is_letter(c) || is_digit(c) || is_hyphen(c)
+        }
+        fn starts_with_letter(c: &str) -> bool {
+            c.chars().next().map(is_letter).unwrap_or(false)
+        }
+        fn ends_with_letter_or_digit(c: &str) -> bool {
+            c.chars()
+                .last()
+                .map(|c| is_letter(c) || is_digit(c))
+                .unwrap_or(false)
+        }
+        fn has_valid_chars(c: &str) -> bool {
+            c.chars().all(is_valid_char)
+        }
         if s.len() > MAX_LABEL_LEN {
             Err(Self::Err::LenLimit)
-        } else {
+        } else if s.len() == 0 {
+            return Ok(Label::null());
+        } else if starts_with_letter(s) && ends_with_letter_or_digit(s) && has_valid_chars(s) {
             Ok(Label(s.to_string().to_lowercase()))
+        } else {
+            Err(Self::Err::InvalidChar)
         }
     }
 }
@@ -56,6 +91,10 @@ mod label_tests {
         // The null label is a valid label
         let label: Label = "".parse().unwrap();
         assert_eq!(label.to_string(), "");
+
+        // whitespaces in label is invalid
+        let label: Result<Label, LabelFromStrErr> = "example ".parse();
+        assert!(label.is_err());
 
         // "example" is a valid label
         let label: Label = "example".parse().unwrap();
