7f106ffe58238204949a8bd30242d2ed28d5b19c
luigiminardim <luigiminardim@gmail.com>
2025-01-15 00:04:20 -0300

    feat: :sparkles: parse zone files records indepently of fields order

--- src/dns/core/zone_file.rs
+++ src/dns/core/zone_file.rs
@@ -3,23 +3,27 @@ use std::{
     str::{Chars, FromStr},
 };
 
-use super::Record;
+use super::{Class, Record, Ttl};
 
+// TODO: Use parser combinator or bnfc
 pub fn parse_str(input: &str) -> Vec<Record> {
     let input = &remove_comments(input);
     let input = &remove_parentesis(input);
     let input = remove_white_space(input);
-    dbg!(&input);
-    input
-        .lines()
-        .filter_map(|line| {
-            Record::from_str(line)
-                .inspect_err(|e| {
-                    dbg!(e, line);
-                })
-                .ok()
-        })
-        .collect::<Vec<Record>>()
+    let mut records = Vec::new();
+    for line in input.lines() {
+        let last_record = records.last();
+        let record = parse_record(line, last_record);
+        match record {
+            Some(record) => {
+                records.push(record);
+            }
+            None => {
+                continue;
+            }
+        }
+    }
+    records
 }
 
 fn remove_comments(input: &str) -> String {
@@ -123,6 +127,69 @@ fn consume_quoted_string(iter: &mut Peekable<Chars>) -> Option<String> {
         }
     }
     Some(result)
+}
+
+fn parse_record(line: &str, last_record: Option<&Record>) -> Option<Record> {
+    let (domain_name_part, rr_part) = line.split_once(' ')?;
+    let domain_name_part = if domain_name_part.is_empty() {
+        last_record
+            .as_ref()
+            .map(|last_record| last_record.name().to_string())
+    } else {
+        Some(domain_name_part.to_string())
+    }?;
+    let (ttl, class, rest) = separate_rr_fields(rr_part, last_record)?;
+    let input = format!("{} {} {} {}", domain_name_part, ttl, class, rest);
+    Record::from_str(&input).ok()
+}
+
+/// Return (ttl, class, ...rest)
+fn separate_rr_fields(
+    rr_part: &str,
+    last_record: Option<&Record>,
+) -> Option<(String, String, String)> {
+    let mut parts = rr_part.split_whitespace().peekable();
+    if let Some(Ok(ttl)) = parts.peek().map(|s| Ttl::from_str(s)) {
+        // If the first part is a number
+        parts.next();
+        if let Some(Ok(class)) = parts.peek().map(|s| Class::from_str(s)) {
+            // If the second part is a class
+            parts.next();
+            let rest = parts.collect::<Vec<&str>>().join(" ");
+            return Some((ttl.to_string(), class.to_string(), rest));
+        } else if let Some(last_record) = last_record {
+            // If the second part is not a class, but the first part is a number
+            let class = last_record.class().to_string();
+            let rest = parts.collect::<Vec<&str>>().join(" ");
+            return Some((last_record.ttl().to_string(), class, rest));
+        } else {
+            return None;
+        }
+    } else if let Some(Ok(class)) = parts.peek().map(|s| Class::from_str(s)) {
+        // If the first part is a class
+        parts.next();
+        if let Some(Ok(ttl)) = parts.peek().map(|s| Ttl::from_str(s)) {
+            // If the second part is a number
+            parts.next();
+            let rest = parts.collect::<Vec<&str>>().join(" ");
+            return Some((ttl.to_string(), class.to_string(), rest));
+        } else if let Some(last_record) = last_record {
+            // If the second part is not a number, but the first part is a class
+            let ttl = last_record.ttl().to_string();
+            let rest = parts.collect::<Vec<&str>>().join(" ");
+            return Some((ttl, class.to_string(), rest));
+        } else {
+            return None;
+        }
+    } else if let Some(last_record) = last_record {
+        // If the first part is not a class nor a number
+        let ttl = last_record.ttl().to_string();
+        let class = last_record.class().to_string();
+        let rest = parts.collect::<Vec<&str>>().join(" ");
+        return Some((ttl, class, rest));
+    } else {
+        return None;
+    }
 }
 
 #[cfg(test)]
@@ -241,5 +308,79 @@ example.com. 0 (
         ];
         let output = parse_str(input);
         assert_eq!(expected, output);
+    }
+
+    #[test]
+    fn test_omit_domain_name() {
+        let input = r#"
+example.com.    0 IN A 127.0.0.1
+                0 IN CNAME cname.com.
+                0 IN TXT "Hello, World!"
+"#;
+        let expected: Vec<Record> = vec![
+            "example.com. 0 IN A 127.0.0.1".parse().unwrap(),
+            "example.com. 0 IN CNAME cname.com.".parse().unwrap(),
+            "example.com. 0 IN TXT \"Hello, World!\"".parse().unwrap(),
+        ];
+        let output = parse_str(input);
+        assert_eq!(expected, output);
+    }
+
+    #[test]
+    fn test_parse_record_with_rr_in_different_orders() {
+        let input = r#"
+; [<domain-name>]   [<TTL>]     [<class>]   <type> <RDATA>
+example.com.        0           IN          A 0.1.1.1
+example.com.        0                       A 0.1.1.0
+example.com.                    IN          A 0.1.0.1
+example.com.                                A 0.1.0.0
+                    0           IN          A 0.0.1.1
+                    0                       A 0.0.1.0
+                                IN          A 0.0.0.1
+                                            A 0.0.0.0  
+
+; [<domain-name>]   [<class>]   [<TTL>]     <type> <RDATA>
+example.com.        IN          0           A 0.1.1.1
+example.com.        IN                      A 0.1.1.0
+example.com.                    0           A 0.1.0.1
+example.com.                                A 0.1.0.0
+                    IN          0           A 0.0.1.1
+                    IN                      A 0.0.1.0
+                                0           A 0.0.0.1
+                                            A 0.0.0.0 
+"#;
+        let expected: Vec<Record> = vec![
+            "example.com. 0 IN A 0.1.1.1",
+            "example.com. 0 IN A 0.1.1.0",
+            "example.com. 0 IN A 0.1.0.1",
+            "example.com. 0 IN A 0.1.0.0",
+            "example.com. 0 IN A 0.0.1.1",
+            "example.com. 0 IN A 0.0.1.0",
+            "example.com. 0 IN A 0.0.0.1",
+            "example.com. 0 IN A 0.0.0.0",
+            "example.com. 0 IN A 0.1.1.1",
+            "example.com. 0 IN A 0.1.1.0",
+            "example.com. 0 IN A 0.1.0.1",
+            "example.com. 0 IN A 0.1.0.0",
+            "example.com. 0 IN A 0.0.1.1",
+            "example.com. 0 IN A 0.0.1.0",
+            "example.com. 0 IN A 0.0.0.1",
+            "example.com. 0 IN A 0.0.0.0",
+        ]
+        .iter()
+        .map(|s| Record::from_str(s).unwrap())
+        .collect();
+        let output = parse_str(input);
+        expected
+            .iter()
+            .zip(output.iter())
+            .for_each(|(expected, output)| {
+                assert_eq!(
+                    expected,
+                    output,
+                    "could't parse \"{}\"",
+                    expected.to_string(),
+                );
+            });
     }
 }
