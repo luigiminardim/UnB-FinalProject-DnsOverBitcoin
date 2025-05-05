f0c1c3be2c707a0dc279d8ad6356af6398ea659d
luigiminardim <luigiminardim@gmail.com>
2025-05-05 10:04:35 -0300

    feat: :sparkles: fetch records from nostr relay

--- dns_nostr_server/src/lib.rs
+++ dns_nostr_server/src/lib.rs
@@ -1,1 +1,3 @@
+pub mod dns_nostr_token_repository;
 pub mod nostr_authority;
+pub mod nostr_events_repository;
--- dns_nostr_server/src/main.rs
+++ dns_nostr_server/src/main.rs
@@ -2,14 +2,25 @@ use hickory_server::{
     authority::{Authority, Catalog},
     ServerFuture,
 };
-use lib::nostr_authority::NostrAuthority;
+use lib::{
+    dns_nostr_token_repository::DnsNostrTokenRepository, nostr_authority::NostrAuthority,
+    nostr_events_repository::NostrEventsRepository,
+};
 use std::sync::Arc;
 use tokio::net::UdpSocket;
 
 #[tokio::main]
 async fn main() {
+    let dns_nostr_token_repository = DnsNostrTokenRepository::new();
+
+    let nostr_events_repository = NostrEventsRepository::new("ws://localhost:8080".to_string());
+
     let mut handler = Catalog::new();
-    let nostr_authority: NostrAuthority = NostrAuthority::new();
+    let nostr_authority: NostrAuthority = NostrAuthority::new(
+        "nostr.dns.name.".parse().unwrap(),
+        dns_nostr_token_repository,
+        nostr_events_repository,
+    );
     handler.upsert(
         nostr_authority.origin().clone(),
         Box::new(Arc::new(nostr_authority)),
