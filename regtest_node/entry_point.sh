015a40db6b6fd289491aa6e2552bff7d49e0ea74
emanuks <emanuels313@gmail.com>
2025-05-31 19:27:36 -0300

    fix: typo

--- regtest_node/entry_point.sh
+++ regtest_node/entry_point.sh
@@ -8,27 +8,27 @@ while [ -z "$IS_RUNNING" ]; do
     IS_RUNNING=$(bitcoin-cli getblockchaininfo)
 done
 
-# create minner wallet if it doesn't exist
-WALLET_EXISTS=$(bitcoin-cli loadwallet "minner" | grep minner)
+# create miner wallet if it doesn't exist
+WALLET_EXISTS=$(bitcoin-cli loadwallet "miner" | grep miner)
 if [ -z "$WALLET_EXISTS" ]; then
-    bitcoin-cli createwallet "minner"
+    bitcoin-cli createwallet "miner"
 fi
 
 
 function mine_block {
-    ADDRESS=$(bitcoin-cli -rpcwallet=minner getnewaddress "" bech32)
+    ADDRESS=$(bitcoin-cli -rpcwallet=miner getnewaddress "" bech32)
     echo "Generated block to address: $ADDRESS"
-    bitcoin-cli -rpcwallet=minner generatetoaddress 1 "$ADDRESS"
+    bitcoin-cli -rpcwallet=miner generatetoaddress 1 "$ADDRESS"
 }
 
-# generate the first 101 blocks to the minner wallet
+# generate the first 101 blocks to the miner wallet
 # this is needed to get the wallet to be able to spend the coins
 CURRENT_BLOCK=$(bitcoin-cli getblockcount)
 for i in $(seq $CURRENT_BLOCK 100); do
     mine_block
 done
 
-# generate blocks to the minner wallet
+# generate blocks to the miner wallet
 while true; do
     sleep 600 # 10 minutes
     mine_block
