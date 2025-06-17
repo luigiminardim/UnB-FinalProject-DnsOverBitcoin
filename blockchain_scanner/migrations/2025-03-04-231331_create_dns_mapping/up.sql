9908f7667b7cc0e02bd10bd96a46b2b0fb506b8b
luigiminardim <luigiminardim@gmail.com>
2025-06-17 09:57:19 -0300

    refactor: :fire: remove old scanner

--- blockchain_scanner/migrations/2025-03-04-231331_create_dns_mapping/up.sql
+++
@@ -1,8 +0,0 @@
--- Your SQL goes here
-CREATE TABLE "dns_mapping" (
-  "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  "tx_id" TEXT NOT NULL,
-  "key" TEXT NOT NULL,
-  "nostr_address" TEXT NOT NULL,
-  "active" BOOLEAN NOT NULL
-);
